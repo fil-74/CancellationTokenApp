a700c140bf447c4eb6a65a55c35f705641f7f821
fil-74 <vlad.filipenko.02@gmail.com>
2022-04-17 21:26:46 +0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationToknApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeout = 0;
+
+            if(args.Length > 0)
+            {
+                Int32.TryParse(args[0], out timeout);
+            }
+            while(timeout <= 0)
+            {
+                Console.WriteLine("Введите время ожидания");
+                if(!Int32.TryParse(Console.ReadLine(), out  timeout) || timeout <= 0)
+                    Console.WriteLine("Введены не корректные данные"); 
+            }
+
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var task = new Task(() => 
+            {
+                Thread.Sleep(100);
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Прервано выполнение задачи");
+                    return;
+                }
+                Console.WriteLine("Задача выполняется");
+                Thread.Sleep(100);
+                Console.WriteLine("Задача завершилась успешно");
+            },token);
+            task.Start();
+            Thread.Sleep(timeout);
+            cts.Cancel();
+            Thread.Sleep(100); 
+            Console.WriteLine(task.Status);
+        }
+    }
+}
